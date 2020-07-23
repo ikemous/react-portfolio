d1dc13b8fe3dc8128a0e6479f3c62ccdd1857393
ike barranco <barranco.ike@outlook.com>
2020-07-23 01:08:20 -0600

    added link references from my last portfolio

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
-const app = express();
+const server = express();
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  server.use(express.static("client/build"));
 }
 
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function(req, res) {
+server.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, function() {
+server.listen(PORT, function() {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
