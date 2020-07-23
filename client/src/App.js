639702f8b71771c8516e6f12a074087aad3d2cc0
ike barranco <barranco.ike@outlook.com>
2020-07-23 06:38:14 -0600

    fixed set page to be able to be used when clicking the li

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import {BrowserRouter as Router, Route} from "react-router-dom";
 import MobileNavBar from "./components/MobileNavBar.js";
 import DesktopNavBar from "./components/DesktopNavBar.js";
 import About from "./pages/About.js";
+import Portfolio from "./pages/Portfolio.js";
+import Contact from "./pages/Contact.js";
 import "./App.css";
 
 function App()
@@ -17,6 +19,8 @@ function App()
       <MobileNavBar>
         <DesktopNavBar page={page} setPage={setPage}/>
         <Route exact path="/About" component={About} />
+        <Route exact path="/Portfolio" component={Portfolio} />
+        <Route exact path="/Contact" component={Contact} />
       </MobileNavBar>
     </Router>
   );
--- client/src/components/DesktopNavBar.js
+++ client/src/components/DesktopNavBar.js
@@ -14,14 +14,14 @@ function DesktopNavBar({page, setPage})
 
             <div className="collapse navbar-collapse" id="myMenu">
                 <ul className="navbar-nav mr-auto mt-2 mt-md-0">
-                    <li className={(page==="About")?"nav-item active": "nav-item"}>
-                        <Link className="nav-link navItem" to="/About"><i className="fas fa-diagnoses" onClick={() => setPage("About")}></i>About</Link>
+                    <li className={(page==="About")?"nav-item active": "nav-item"}  onClick={() => setPage("About")}>
+                        <Link className="nav-link navItem" to="/About"><i className="fas fa-diagnoses"></i>About</Link>
                     </li>
-                    <li className={(page==="Portfolio")?"nav-item active": "nav-item"}>
-                        <Link className="nav-link navItem" to="/Portfolio"><i className="fas fa-book-open" onClick={() => setPage("Portfolio")}></i>Portfolio</Link>
+                    <li className={(page==="Portfolio")?"nav-item active": "nav-item"} onClick={() => setPage("Portfolio")}>
+                        <Link className="nav-link navItem" to="/Portfolio"><i className="fas fa-book-open"></i>Portfolio</Link>
                     </li>
-                    <li className={(page==="Contact")?"nav-item active": "nav-item"}>
-                        <Link className="nav-link navItem" to="/Contact"><i className="fas fa-envelope-open" onClick={() => setPage("Contact")}></i>Contact</Link>
+                    <li className={(page==="Contact")?"nav-item active": "nav-item"}  onClick={() => setPage("Contact")}>
+                        <Link className="nav-link navItem" to="/Contact"><i className="fas fa-envelope-open"></i>Contact</Link>
                     </li>
                 </ul>
             </div>
