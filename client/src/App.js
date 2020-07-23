a7e59f3d2de60ff24caedc2cc37a4a753b5efc81
ike barranco <barranco.ike@outlook.com>
2020-07-23 01:36:36 -0600

    added router to the app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from "react";
+import {BrowserRouter as Router, Route} from "react-router-dom";
 import "./App.css";
 
 class App extends Component {
