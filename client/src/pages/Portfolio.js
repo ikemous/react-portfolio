c87f8b0078da63d8e9fc2c19886ca1c7f8a474ad
ike barranco <barranco.ike@outlook.com>
2020-07-24 07:16:51 -0600

    added use effect for api queries

--- client/src/pages/Portfolio.js
+++ client/src/pages/Portfolio.js
@@ -1,11 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import API from "../utils/API.js";
 import Form from "../components/portfolioComponents/Form.js";
 import {Container, Card} from "semantic-ui-react";
 
 function Portfolio()
 {
     const [filter, setFilter] = useState("all");
-    
+
+    useEffect(() => {
+        API.getProjects()
+        .then(collections => {
+            console.log(collections.data)
+        })
+        .catch(error => console.log(error));
+    }, [filter]);
     return (
         <Container>
             <Form />
