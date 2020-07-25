911d9e8f82c0149c9a37e0751f597f0ae996b39d
ike barranco <barranco.ike@outlook.com>
2020-07-24 23:20:33 -0600

    added cards for the projects in a component

--- client/src/components/portfolioComponents/Form.js
+++ client/src/components/portfolioComponents/Form.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {Dropdown} from "semantic-ui-react";
+import {Link} from "react-router-dom";
 
 const options = [
     { key: 'All', text: 'Show All', value: '' },
@@ -28,7 +29,7 @@ function Form({setFilter})
         >
             <Dropdown.Menu>
                 <Dropdown.Header icon='tags' content='Filter by skill' />
-                {options.map(option => <Dropdown.Item key={option.key} value={option.value} onClick={() => setFilter(option.value)}>{option.text}</Dropdown.Item>)}
+                {options.map(option => <Dropdown.Item as={Link} key={option.key} value={option.value} onClick={() => setFilter(option.value)}>{option.text}</Dropdown.Item>)}
             </Dropdown.Menu>
         </Dropdown>
     );
--- client/src/components/portfolioComponents/ProjectCard.js
+++ client/src/components/portfolioComponents/ProjectCard.js
@@ -1,13 +1,24 @@
 import React from "react";
-import {Card} from "semantic-ui-react";
+import {Card, Image, Icon} from "semantic-ui-react";
 
-function ProjectCard(props)
+function ProjectCard({imageURL, title, description, skills, githubURL, deployedURL})
 {
     return (
         <Card>
-            {props.title}
+            <Image src={imageURL || "https://via.placeholder.com/150"} wrapped ui={false} />
+            <Card.Content>
+                <Card.Header>{title}</Card.Header>
+                <Card.Meta>{skills}</Card.Meta>
+                <Card.Description>{description}</Card.Description>
+                <Card.Content>
+                    <a href={githubURL}><Icon name="code"></Icon>Code Repository</a>
+                </Card.Content>
+                <Card.Content>
+                    <a href={deployedURL}><Icon name="world"></Icon>Deployed Application</a>
+                </Card.Content>
+            </Card.Content>
         </Card>
-    )
+    );
 };
 
 export default ProjectCard;
