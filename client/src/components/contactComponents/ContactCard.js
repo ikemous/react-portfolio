251a2251e6724a05469635260c07091e50bcd5bb
ike barranco <barranco.ike@outlook.com>
2020-07-26 00:59:51 -0600

    modified grid columns for desktop version

--- client/src/components/contactComponents/ContactCard.js
+++ client/src/components/contactComponents/ContactCard.js
@@ -9,7 +9,7 @@ function ContactCard()
             <Card.Header as="h2" textAlign="center">Contact Me</Card.Header>
             <Card.Content>
                 <Grid container>
-                    <Grid.Column  mobile={16} tablet={8} computer={4}>
+                    <Grid.Column  mobile={16} tablet={8} computer={5}>
                         <Segment textAlign="center" basic><Icon name="clock" size="big" />Contact Times MST</Segment>
                         <Segment textAlign="center" basic>
                             <h3>Sun-Mon</h3>
@@ -19,11 +19,11 @@ function ContactCard()
                             <p>6:30 AM - 8:30 AM</p>
                         </Segment>
                     </Grid.Column>
-                    <Grid.Column  mobile={16} tablet={8} computer={4}>
+                    <Grid.Column  mobile={16} tablet={8} computer={6}>
                         <Segment textAlign="center" basic><Icon name="phone square" size="big" />Phone</Segment>
                         <Segment textAlign="center" basic><h3>385-229-7480</h3></Segment>
                     </Grid.Column>
-                    <Grid.Column  mobile={16} tablet={8} computer={4}>
+                    <Grid.Column  mobile={16} tablet={8} computer={5}>
                         <Segment textAlign="center" basic><Icon name="mail" size="big" />Email</Segment>
                         <Segment textAlign="center" basic><h3><a  href="mailto:barranco.ike@gmail.com?subject=Email%20Regarding%20Portfolio">barranco.ike@gmail.com</a></h3></Segment>
                     </Grid.Column>
