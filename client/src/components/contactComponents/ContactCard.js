b81a01a68ae785b1db0864374d06bd7563c3bb0e
ike barranco <barranco.ike@outlook.com>
2020-07-25 23:44:30 -0600

    altered contact card to be more mobile friendly

--- client/src/components/contactComponents/ContactCard.js
+++ client/src/components/contactComponents/ContactCard.js
@@ -8,18 +8,24 @@ function ContactCard()
         <Card fluid>
             <Card.Header as="h2" textAlign="center">Contact Me</Card.Header>
             <Card.Content>
-                <Grid container columns={3}>
-                    <Grid.Column>
-                        <Segment textAlign="center" basic><Icon name="clock" size="big" />Contact Times</Segment>
-                        <Segment textAlign="center" basic>Contact Times</Segment>
+                <Grid container>
+                    <Grid.Column  mobile={16} tablet={8} computer={4}>
+                        <Segment textAlign="center" basic><Icon name="clock" size="big" />Contact Times MST</Segment>
+                        <Segment textAlign="center" basic>
+                            <h3>Sun-Mon</h3>
+                            <p>6:30 AM - 8:30 AM</p>
+                            <p>5:00 PM - 9:30 PM</p>
+                            <h3>Fri-Sat</h3>
+                            <p>6:30 AM - 8:30 AM</p>
+                        </Segment>
                     </Grid.Column>
-                    <Grid.Column>
-                        <Segment textAlign="center" basic><Icon name="phone square" size="big" />Email</Segment>
-                        <Segment textAlign="center" basic>Contact Times</Segment>
+                    <Grid.Column  mobile={16} tablet={8} computer={4}>
+                        <Segment textAlign="center" basic><Icon name="phone square" size="big" />Phone</Segment>
+                        <Segment textAlign="center" basic><h3>385-229-7480</h3></Segment>
                     </Grid.Column>
-                    <Grid.Column>
-                        <Segment textAlign="center" basic><Icon name="mail" size="big" />Phone</Segment>
-                        <Segment textAlign="center" basic>Contact Times</Segment>
+                    <Grid.Column  mobile={16} tablet={8} computer={4}>
+                        <Segment textAlign="center" basic><Icon name="mail" size="big" />Email</Segment>
+                        <Segment textAlign="center" basic><h3><a  href="mailto:barranco.ike@gmail.com?subject=Email%20Regarding%20Portfolio">barranco.ike@gmail.com</a></h3></Segment>
                     </Grid.Column>
                 </Grid>
             </Card.Content>
