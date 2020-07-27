5131246bb65f65039356171fb4189d1a80063e00
ike barranco <barranco.ike@outlook.com>
2020-07-26 20:39:33 -0600

    moved css for components

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,12 @@
 /* eslint-disable react/prefer-stateless-function */
 import React, { useState } from "react";
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch, useLocation} from "react-router-dom";
 import MobileNavBar from "./components/MobileNavBar.js";
 import DesktopNavBar from "./components/DesktopNavBar.js";
 import About from "./pages/About.js";
 import Portfolio from "./pages/Portfolio.js";
 import Contact from "./pages/Contact.js";
 import NotFound from "./pages/Notfound.js";
-import "./App.css";
 
 function App()
 {
