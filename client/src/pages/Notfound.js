6a8228de66aff5551d716c5f6c9efdd1d701ec3f
ike barranco <barranco.ike@outlook.com>
2020-07-26 18:20:27 -0600

    added aria label to emoji faces

--- client/src/pages/Notfound.js
+++ client/src/pages/Notfound.js
@@ -6,7 +6,7 @@ function NotFound()
 {
     return (
         <Segment textAlign="center" basic>
-            <h2>😭 Page Not Found 😭</h2>
+            <h2><span role="image" aria-label="Crying Face Emoji">😭</span> Page Not Found <span role="image" aria-label="Crying Face Emoji">😭</span></h2>
             <p>Click <Link to={"/about"}>Here</Link> Or Use The Navigation Bar</p>
         </Segment>
     );
