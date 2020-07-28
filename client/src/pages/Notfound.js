3795c9a9019a24ebdc37624de7cd45a89880b9c9
ike barranco <barranco.ike@outlook.com>
2020-07-27 20:18:59 -0600

    removed styles

--- client/src/pages/Notfound.js
+++ client/src/pages/Notfound.js
@@ -1,12 +1,11 @@
 import React from "react";
 import {Link} from "react-router-dom";
 import {Segment} from "semantic-ui-react";
-import "./notFoundStyle.css";
 
 function NotFound()
 {
     return (
-        <Segment textAlign="center" basic>
+        <Segment textAlign="center" basic style={{color: "#4183c4"}}>
             <h2><span role="img" aria-label="crying">😭</span> Page Not Found <span role="img" aria-label="crying">😭</span></h2>
             <p>Click <Link to={"/about"}>Here</Link> Or Use The Navigation Bar</p>
         </Segment>
