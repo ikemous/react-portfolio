fcd831014bf55cf811208fe1022959e6e1bc2e78
ike barranco <barranco.ike@outlook.com>
2020-07-28 03:20:34 -0600

    added style sheet for the social items to rotate icons

--- client/src/components/contactComponents/SocialItem.js
+++ client/src/components/contactComponents/SocialItem.js
@@ -1,12 +1,13 @@
 import React from "react";
 import {List, Icon} from "semantic-ui-react";
+import "./socialItemStyle.css";
 
 function SocialItem({name, color, socialName, link})
 {
     return (
         <List.Item>
             <a href={link} rel="noopener noreferrer" target="_blank">
-                <Icon name={name} color={color} size="large" />
+                <Icon className="socialIcon" name={name} color={color} size="large" />
                 {socialName}
             </a>
         </List.Item>
